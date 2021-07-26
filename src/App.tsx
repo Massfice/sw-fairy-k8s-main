24181b23f3882037fd1a4a98c6b751f9b7de2950
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-07-26 23:25:14 +0200

    Making module federation work

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+//@ts-ignore
+import CounterAppOne from "app1/CounterAppOne";
+
+const App = () => (
+  <div style={{ margin: "20px" }}>
+    <React.Suspense fallback="Loading header...">
+      <div
+        style={{
+          border: "1px dashed black",
+          height: "50vh",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>CONTAINER</h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+          }}
+        >
+          <div
+            style={{
+              marginRight: "2rem",
+              padding: "2rem",
+              border: "1px dashed black",
+            }}
+          >
+            <h2>APP-1</h2>
+            <CounterAppOne />
+          </div>
+        </div>
+      </div>
+    </React.Suspense>
+  </div>
+);
+
+export default App;
