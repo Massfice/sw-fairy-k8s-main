c0084f9fafcabcc8f8905e80f5d6a5cb425dfdc5
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-07-27 00:33:37 +0200

    Bringing back dev files

---
+++ gulpfile.ts
@@ -0,0 +1,11 @@
+import { deployKubernetes, configs } from '@massfice/sw-fairy-common-deploy-scripts';
+
+const config = configs.kubernetes(
+    __dirname,
+    'https://9766b21f-9055-436e-a20a-8018422119a5.k8s.ondigitalocean.com',
+    'sw-fairy-k8s-main',
+);
+
+const deploy = deployKubernetes(config);
+
+export { deploy };
